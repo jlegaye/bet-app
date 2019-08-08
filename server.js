e7d7ab0a41698874bf00de8d8ec4f58c2dbad4fc
Johann Legaye <johann.legaye@gmail.com>
2019-08-08 20:53:42 +0200

    express server + angularjs skeleton

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 
 // set the home page route
 app.get('/', function(req, res) {
